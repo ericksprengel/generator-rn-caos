22b2a565c14b34669ad2222997219a589ce98ba8
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-12-30 16:02:58 -0300

    containers: use componentStates on container's test

--- generators/app/templates/container/tests/index.test.js
+++ generators/app/templates/container/tests/index.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import <%= componentName %> from '.'
+import testSnapshots from 'src/test/helpers/testSnapshots'
+import * as states from './componentStates'
+import <%= componentName %> from '..'
 
 describe('<%= componentName %> container', () => {
-  testSnapshots(
-    states
-  )
+  testSnapshots(states)
 
 <% for (const action of actions) { %>
   test('<%= action %> should be called', () => {
--- tests/src/containers/App/Auth/Login/tests/index.test.js
+++ tests/src/containers/App/Auth/Login/tests/index.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Login from '.'
+import testSnapshots from 'src/test/helpers/testSnapshots'
+import * as states from './componentStates'
+import Login from '..'
 
 describe('Login container', () => {
-  testSnapshots(
-    states
-  )
+  testSnapshots(states)
 
 
   test('onLogin should be called', () => {
