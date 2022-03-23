2b8ea7f16a7bac59f529ce6b404c3cb0b7be0fd2
Nathan Veira <dev.goytai@gmail.com>
2022-03-23 19:27:07 -0300

    fix: tests warnings

--- generators/app/templates/features/screens/tests/index.test.tsx
+++ generators/app/templates/features/screens/tests/index.test.tsx
@@ -1,27 +1,41 @@
 import React from 'react'
 
-import { create, act, ReactTestInstance } from 'react-test-renderer'
+import { create, act, ReactTestInstance, ReactTestRenderer } from 'react-test-renderer'
 import { NavigationStackProp } from 'react-navigation-stack'
 import { mock } from 'jest-mock-extended'
 
 import { <%= screenName %>Container } from 'src/features/<%= featureName %>/screens/<%= screenName %>/ui'
 import { <%= screenName %>Screen } from '..'
 
-describe('<%= screenName %>Screen screen', () => {
-  const mockNavigate = jest.fn()
-  const mockNavigationPop = jest.fn()
-  const navigationMock = mock<NavigationStackProp>({
-    navigate: mockNavigate,
-    pop: mockNavigationPop,
+const mockNavigate = jest.fn()
+const mockNavigationGoBack = jest.fn()
+const navigationMock = mock<NavigationStackProp>({
+  navigate: mockNavigate,
+  goBack: mockNavigationGoBack,
+})
+
+const getContainerInstance = async (): Promise<ReactTestInstance> => {
+  let renderer: ReactTestRenderer | undefined
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  await act(async () => {
+    renderer = create(<<%= screenName %>Screen navigation={navigationMock} />)
   })
 
+  if (!renderer) {
+    throw new Error('renderer not defined')
+  }
+
+  return renderer.root.findByType(<%= screenName %>Container)
+}
+
+describe('<%= screenName %>Screen screen', () => {
   let containerInstance: ReactTestInstance
 
-  beforeEach(() => {
+  beforeEach(async () => {
     jest.clearAllMocks()
-    containerInstance = create(
-      <<%= screenName %>Screen navigation={navigationMock} />,
-    ).root.findByType(<%= screenName %>Container)
+
+    containerInstance = await getContainerInstance()
   })
 <% for (const action of actions) { -%>
 
